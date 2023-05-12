76ce9108d8fa30149d5a1ad8253fc81ffe0a58b6
ANONYMOUS-PC\adi_n <adityasnavale@gmail.com>
2023-05-12 11:47:53 -0700

    Imported snake game

---
+++ Keil/Objects/retarget.d
@@ -0,0 +1,12 @@
+.\objects\retarget.o: ..\Files\Software\device\retarget.c
+.\objects\retarget.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+.\objects\retarget.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\time.h
+.\objects\retarget.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\rt_misc.h
+.\objects\retarget.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+.\objects\retarget.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\retarget.o: ..\Files\Software\device\EDK_CM0.h
+.\objects\retarget.o: ..\Files\Software\device\edk_driver.h
+.\objects\retarget.o: ..\Files\Software\core\core_cm0.h
+.\objects\retarget.o: ..\Files\Software\core\core_cmInstr.h
+.\objects\retarget.o: ..\Files\Software\core\core_cmFunc.h
+.\objects\retarget.o: ..\Files\Software\device\edk_api.h
